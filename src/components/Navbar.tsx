4ece0a6a397955835c1bd180d4a91273cd908cf5
Helen Dee <helendeee12@gmail.com>
2025-07-26 04:38:50 +0100

    first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+// const navLinks = [
+//     {label:"Home", path: "/home"}
+// ]
+const Navbar = () => {
+  return (
+    <div className=''>
+        <h1 className='font-[font-playfair] text-4xl text-center text-white bg-black p-4'>
+            HELEN
+        </h1>
+    </div>
+  )
+}
+
+export default Navbar
