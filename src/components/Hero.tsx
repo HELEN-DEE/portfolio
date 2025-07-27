7a43926f478875d616cac46c4876629adf289dce
Helen Dee <helendeee12@gmail.com>
2025-07-27 23:13:45 +0100

    Added Info style

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,8 +7,8 @@ const Hero = () => {
         <h1 className='text-5xl font-bold mt-10'>
             Frontend Developer
         </h1>
-        <p>
-            I build fast, responsive, and visually polished web apps — focused on clean code, smooth interactions, and real-world impact.
+        <p className='text-lg mt-4'>
+            I build fast, responsive, and visually polished web apps focused on clean code, smooth interactions, and real-world impact.
         </p>
     </div>
    </section>
